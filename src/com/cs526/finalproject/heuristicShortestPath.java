090b63fd7bde87cedfd10b1590fc9e9c54e18236
Michael Burke <mikeb@burst.com>
2017-04-06 17:00:19 -0400

    updates

--- src/com/cs526/finalproject/heuristicShortestPath.java
+++ src/com/cs526/finalproject/heuristicShortestPath.java
@@ -15,15 +15,14 @@ import java.util.Map;
  *
  */
 public class heuristicShortestPath {
-	private static final String END_NODE = "Z";
+	private static final String END_NODE = "Z"; 	// End node is hardcoded								
 	
-	private static Map<String, Integer> directDistances = new HashMap<>();
-	private static AdjacencyList graph = new AdjacencyList();
-	private static String startNode = "J";
+	private static Map<String, Integer> directDistances = new HashMap<>();	// Holds direct distances
+	private static AdjacencyList graph = new AdjacencyList();				// Main graph data structure
+	private static String startNode = "J";									// Start node passed in as arg
 	
 	public static void main(String[] args) {
-		// Path hardcoded to distance.txt
-		directDistances = readDirectDistancesFromFile();
+		directDistances = readDirectDistancesFromFile();					// Path hardcoded to distance.txt
 		
 		// Path set by passing in a command line arg of the path.
 		graph = readGraphFromFile(args[0]);
@@ -61,70 +60,65 @@ public class heuristicShortestPath {
 		
 	}
 	
+	/**
+	 * Sets the edge weights given a graph and a Map of the distances
+	 * @param list - the Adjacency List for the graph to be worked on
+	 * @param distances - a Map<String, Integer> of the Key Value pairs where
+	 * nodes are the vertex names and the Values are the Integers of the distance.
+	 * @precondition - inputs should have equal numbers of items
+	 */
 	private static void setEdgeWeights(AdjacencyList list, Map<String, Integer> distances) {
+		/* 
+		 * If these sizes don't match, something is wrong with the input
+		 * and we should end the program.
+		 */
 		if (list.numVertices() != distances.size()) {
-			System.out.println("List numvertices = " + list.numVertices());
-			System.out.println("Distances.size() = " + distances.size());
 			throw new IllegalArgumentException("The data does not match.");
 		}
 		
+		/*
+		 *  Iterate over the keys of distances.
+		 *  Set the distance for each key.
+		 */
 		for (String name : distances.keySet()) {
 			list.findVertex(name).setDirectDistanceToZ(distances.get(name));;
 		}
 	}
 	
+	/**
+	 * This method reads in a text file of a graph representation and turns it
+	 * into an AdjacencyList. This method is highly inefficient due to 
+	 * multiple for loops, but a specification of this project is that there are
+	 * only a maximum of 26 nodes, so the data will never get large.
+	 * @param filePath - the path to read in the graph input file
+	 * @return - a new AdjacencyList filled with the data from the input file
+	 */
 	private static AdjacencyList readGraphFromFile(String filePath) {
+		// We will return this temp AdjacencyList
 		AdjacencyList temp = new AdjacencyList();
+		
+		/* This is our intermediary data structure for transferring data
+		 * from the file to our AdjacencyList
+		 */
+		List<String[]> dataStore = new ArrayList<>();
+		
+		// File reading utils
 		BufferedReader br = null;
 		FileReader fr = null;
 		
+		// Try/catch the file reading process
 		try {
 			fr = new FileReader(filePath);
 			br = new BufferedReader(fr);
 			String currentLine;
-			List<String[]> dataStore = new ArrayList<>();
 			
 			/*
-			 *  First, read each line and add as an entry into dataStore
-			 *  Using an ArrayList of ArrayList<String> for ease of handling
+			 *  First, read each line, split it on whitespace
+			 *  and add as an entry into dataStore
+			 *  using an ArrayList<String[]> for ease of handling
 			 */
 			while ((currentLine = br.readLine()) != null) {
 				dataStore.add((String[])currentLine.split("\\s+"));
-			}
-			
-			// Then iterate through entries and create AdjacencyList
-			for (int i = 0; i < dataStore.size(); i++) {
-				/*
-				 *  Because of the format of the input file, the first line will be shorter than the rest.
-				 *  So here we are adding one element at the front of line 1
-				 */
-				if (i == 0) {
-					String[] tempArray = new String[dataStore.get(0).length + 1];
-					tempArray[0] = "_";
-					for (int j = 1; j < tempArray.length; j++) {
-						tempArray[j] = dataStore.get(0)[j-1];
-					}
-					dataStore.set(0, tempArray);
-					continue; 
-				}
-				
-				Vertex v = null;
-				for (int j = 0; j < dataStore.get(i).length; j++) {
-					if (j == 0) {
-						v = new Vertex(dataStore.get(i)[0]);			// Create new vertex with name of row
-						continue;
-					}
-					if (Integer.parseInt(dataStore.get(i)[j]) != 0) {
-						Edge newEdge = new Edge(
-								v.getName(),
-								dataStore.get(0)[j],
-								Integer.parseInt(dataStore.get(i)[j])
-								);
-						v.addNewEdge(newEdge);
-					}
-				}
-				// Add new vertex to temp AdjacencyList
-				temp.addVertex(v);
 			}
 			
 		} catch (IOException e) {
@@ -141,6 +135,52 @@ public class heuristicShortestPath {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+			
+		// Then iterate through entries and create AdjacencyList
+		for (int i = 0; i < dataStore.size(); i++) {
+			/*
+			 *  Because of the format of the input file, the first line will be shorter than the rest.
+			 *  So here we are adding one element at the front of line 1. This will make it easy
+			 *  for us to correlate the columns.
+			 */
+			if (i == 0) {
+				String[] tempArray = new 
+						String[dataStore.get(0).length + 1];	// Create a new slightly larger array
+				tempArray[0] = "_";								// Add our placeholder item
+				for (int j = 1; j < tempArray.length; j++) {	// Add all of our elements after it
+					tempArray[j] = dataStore.get(0)[j-1];
+				}
+				dataStore.set(0, tempArray);					// save our temparray to the datastore
+				continue; 
+			}
+			
+			Vertex v = null;										// Create new vertex for this entry
+			for (int j = 0; j < dataStore.get(i).length; j++) {		// Loop through this entry
+				/*
+				 * If we are in the first column, we know we don't have
+				 * ints but we have vertex names. So, we create a new 
+				 * vertex with the name of the row.
+				 */
+				if (j == 0) {										
+					v = new Vertex(dataStore.get(i)[0]);			
+					continue;
+				}
+				
+				/*
+				 * Now we add each edge to the Vertex.
+				 */
+				if (Integer.parseInt(dataStore.get(i)[j]) != 0) {
+					Edge newEdge = new Edge(
+							v.getName(),
+							dataStore.get(0)[j],
+							Integer.parseInt(dataStore.get(i)[j])
+							);
+					v.addNewEdge(newEdge);
+				}
+			}
+			// Finally we add our vertex to our AdjacencyList
+			temp.addVertex(v);
 		}
 		
 		// Return AdjacencyList
